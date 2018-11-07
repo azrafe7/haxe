508cec005df9adc0bd18d4eedd54eeeb723b8f39
azrafe7 <azrafe7@gmail.com>
2018-11-07 15:15:30 +0100

    start integrating ECharts

--- tests/benchs/src/Chart.hx
+++ tests/benchs/src/Chart.hx
@@ -1,10 +1,22 @@
 import ChartMacro;
 import haxe.Json;
+import js.Browser;
 
 import BenchDB.TestResultInfo;
 
 using StringTools;
 using Lambda;
+
+
+@:native("echarts")
+extern class EChartsInit {
+	static public function init(dom:Dynamic):ECharts;
+}
+
+@:native("ECharts")
+extern class ECharts {
+	public function setOption(options:Dynamic):Dynamic;
+}
 
 
 class Chart {
@@ -18,12 +30,41 @@ class Chart {
 		benchDB = new BenchDB(jsonArray);
 
 		var prettyJson = benchDB.toJson("  ");
+	#if sys
 		sys.io.File.saveContent("benchdb.json", prettyJson);
+	#end
 
 		trace(benchDB.suiteNames);
 
-		var results = benchDB.query({suiteName:"Custom(Array<Int>[1000])", target:null});
+		var results = benchDB.query({suiteID:benchDB.suiteNames["json"]});
 		trace(haxe.format.JsonPrinter.print(results, null, "  "));
 		trace(results.length);
+
+		var chartDiv = Browser.document.getElementById("chart");
+		trace(chartDiv);
+		var chart = EChartsInit.init(chartDiv);
+
+
+		var option = {
+			title: {
+					text: 'ECharts entry example'
+			},
+			tooltip: {},
+			legend: {
+					data:['Sales']
+			},
+			xAxis: {
+					data: ["shirt","cardign","chiffon shirt","pants","heels","socks"]
+			},
+			yAxis: {},
+			series: [{
+					name: 'Sales',
+					type: 'bar',
+					data: [5, 20, 36, 10, 10, 20]
+			}]
+		};
+
+		// use configuration item and data specified to show chart
+		chart.setOption(option);
 	}
 }
